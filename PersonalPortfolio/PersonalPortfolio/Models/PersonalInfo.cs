764ae2e4d762eb6e4808c60ded5d502c0c8e4a37
mdodson1995 <m.dodson@hotmail.com>
2018-01-11 18:22:47 -0500

    Added Base Solution and Add Personal Model

---
+++ PersonalPortfolio/PersonalPortfolio/Models/PersonalInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace PersonalPortfolio.Models
+{
+    public class PersonalInfo
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Name")]
+        public string name { get; set; }
+
+        [Display(Name = "Primary Phone No.")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Must be a Valid Phone Number.")]
+        public string PrimaryPhone { get; set; }
+
+        [Display(Name = "Primary Email.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Must be a Valid Email Address.")]
+        public string PrimaryEmail { get; set; }
+    }
+}
