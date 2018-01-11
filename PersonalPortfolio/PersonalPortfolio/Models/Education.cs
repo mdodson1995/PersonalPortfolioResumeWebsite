aaa0abb1b4f2ae4b4e2bc165c3362bdd4f71618e
mdodson1995 <m.dodson@hotmail.com>
2018-01-11 18:46:11 -0500

    Added Education Model

---
+++ PersonalPortfolio/PersonalPortfolio/Models/Education.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace PersonalPortfolio.Models
+{
+    public class Education
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "School Name")]
+        public string SchoolName { get; set; }
+
+        [Display(Name = "City")]
+        public string SchoolCity { get; set; }
+
+        [Display(Name = "State")]
+        public string SchoolState { get; set; }
+
+        [Display(Name = "Zip Code")]
+        [DataType(DataType.PostalCode, ErrorMessage = "Must be a Valid Zip Code.")]
+        public string SchoolZip { get; set; }
+    }
+}
