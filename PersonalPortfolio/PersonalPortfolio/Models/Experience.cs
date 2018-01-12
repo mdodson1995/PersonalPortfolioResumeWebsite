da8df11a6f43b709022cf1c116543249cd9950db
mdodson1995 <m.dodson@hotmail.com>
2018-01-11 19:38:05 -0500

    Education Model

---
+++ PersonalPortfolio/PersonalPortfolio/Models/Experience.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace PersonalPortfolio.Models
+{
+    public class Experience
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Job/Internship")]
+        public string ExperienceName { get; set; }
+
+        [Display(Name = "City")]
+        public string CityHeld { get; set; }
+
+        [Display(Name = "State")]
+        public string StateHeld { get; set; }
+
+        [Display(Name = "Zip Code")]
+        [DataType(DataType.PostalCode)]
+        public string ZipHeld { get; set; }
+
+        [Display(Name = "Duration of Job")]
+        public string MonthsHeld { get; set; }
+
+        [Display(Name = "Description of Job")]
+        public string descriptionOfJob { get; set; }
+    }
+}
